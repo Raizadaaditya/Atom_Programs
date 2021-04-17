e1adea68795856bf21dc36e5eace4bdbe350b0ac
AdityaRaizada <raizada.aditya@yahoo.com>
2021-04-17 20:01:10 +0530

    Succes

--- weekday_of_year.py
+++ weekday_of_year.py
@@ -3,13 +3,13 @@ finding the weekday of perticular date
 '''
 
 from datetime import datetime as dt
-import caledar as cl
+import calendar as cl
 week = {0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
-date_txt = input()
-date_obj = dt.strptime(date_txt, "%m %d %Y")
+date_txt = input('Enter Date in format MM-DD-YYY: ')
+date_obj = dt.strptime(date_txt, "%m-%d-%Y")
 date_year = date_obj.year
-date_month = date.month
-date_day = date.day
+date_month = date_obj.month
+date_day = date_obj.day
 
 weekday = cl.weekday(date_year, date_month, date_day)
 
