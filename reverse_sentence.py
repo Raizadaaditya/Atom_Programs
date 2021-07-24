97f580fb2b4f08f546b66b3de429fe3b6f70858b
Raizadaaditya <raizada.aditya@yahoo.com>
2021-07-24 20:04:25 +0530

    changes done in reverse string program

--- reverse_sentence.py
+++ reverse_sentence.py
@@ -1,5 +1,18 @@
+'''
+reverse a string function
+
+'''
+def my_function(x):
+  return x[::-1]
+
+mytxt = my_function("I wonder how this text looks like backwards")
+
+print(mytxt)
+
+'''
+2nd way
 sentence = "I like coding in python"
-reversed_sent = ""
-for ch in range(len(sentence)-1, -1, -1):
-    reversed_sent += sentence[ch]
+reversed_sent = "I like coding in python"[::-1]
+
 print(reversed_sent)
+'''
